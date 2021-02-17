d43ed9e480636dbfe3fce579662807006964002b
Wataru Ito <5068789+wataruito@users.noreply.github.com>
2021-02-17 17:08:27 -0500

    Delete debug.py

--- debug.py
+++
@@ -1,9 +0,0 @@
-# import pandas as pd
-import edit_labels as vp
-h5_path = r'W:\videos_synchrony\20200713\m154DLC_resnet50_test01Dec21shuffle1_100000.h5'
-# df = pd.read_hdf(path)
-
-
-video = r'm154.mp4'
-
-vp.edit_labels(h5_path, video, 2)
